3d5b15d3c581227d4c494d52207d791cb00493d9
Dao Duy Quang <daoduyquang91@gmail.com>
2016-04-06 19:38:33 +0700

    Initial Commit

---
+++ faces/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  faces
+//
+//  Created by Duy Quang on 4/6/16.
+//  Copyright © 2016 duyquang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ faces/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  faces
+//
+//  Created by Duy Quang on 4/6/16.
+//  Copyright © 2016 duyquang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
